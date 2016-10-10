0c1e5d001759b0465845910da0c1c952cb089775
嵇明新 <493972613@qq.com>
2016-10-10 17:17:58 +0800

    首页添加栏目View

---
+++ TodayNewsTest/YMScrollTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  YMScrollTitleView.swift
+//  TodayNewsTest
+//
+//  Created by 嵇明新 on 2016/10/10.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+import UIKit
+
+class YMScrollTitleView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
