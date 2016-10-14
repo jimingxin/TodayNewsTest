14f7d39357e6a8ba07786140d9dad4f3ec907590
嵇明新 <493972613@qq.com>
2016-10-14 12:32:51 +0800

    视频界面完成Topic

---
+++ TodayNewsTest/YMVideoTitleView.swift
@@ -0,0 +1,13 @@
+//
+//  YMVideoTitleView.swift
+//  TodayNewsTest
+//
+//  Created by 嵇明新 on 2016/10/13.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+import Cocoa
+
+class YMVideoTitleView: UIView {
+
+}
---
+++ TodayNewsTest/YMVideoTopicCell.swift
@@ -0,0 +1,24 @@
+//
+//  YMVideoTopicCell.swift
+//  TodayNewsTest
+//
+//  Created by 嵇明新 on 2016/10/14.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+import UIKit
+
+class YMVideoTopicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
