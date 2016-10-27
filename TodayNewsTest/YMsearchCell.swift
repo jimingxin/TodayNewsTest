5ca49fc68831f8c9bc3c63124679d6803eb9f51d
嵇明新 <493972613@qq.com>
2016-10-27 17:07:49 +0800

    关注界面开始写代码

---
+++ TodayNewsTest/YMsearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  YMsearchCell.swift
+//  TodayNewsTest
+//
+//  Created by 嵇明新 on 2016/10/27.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+import UIKit
+
+class YMsearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
